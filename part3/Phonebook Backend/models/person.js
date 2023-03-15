b5f6a10ee847276013ecc6def3cde670b00d2cf3
ritatanght <doggymint@gmail.com>
2023-03-15 13:05:40 -0400

    submit 3.22

--- part3/Phonebook Backend/models/person.js
+++ part3/Phonebook Backend/models/person.js
@@ -4,11 +4,10 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const url = process.env.MONGODB_URI;
-//console.log("connecting to", url);
 
 mongoose
   .connect(url)
-  .then((result) => {
+  .then(() => {
     console.log("connected to MongoDB");
   })
   .catch((error) => {
